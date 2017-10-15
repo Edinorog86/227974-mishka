ba541230d8145f4900420a7fe784a742f043f71e
Surho <surho007@hotmail.com>
2017-10-15 18:05:14 +0300

    some automatisation

--- gulpfile.js
+++ gulpfile.js
@@ -4,8 +4,48 @@ var gulp = require("gulp");
 var sass = require("gulp-sass");
 var plumber = require("gulp-plumber");
 var postcss = require("gulp-postcss");
+var posthtml = require("gulp-posthtml");
+var include = require("posthtml-include");
 var autoprefixer = require("autoprefixer");
 var server = require("browser-sync").create();
+var minify = require("gulp-csso");
+var rename = require("gulp-rename");
+var imagemin = require("gulp-imagemin");
+var imageminWebp = require("gulp-webp");
+var svgstore = require("gulp-svgstore");
+
+gulp.task("html", function() {
+  return gulp.src("*.html")
+    .pipe(posthtml([
+      include()
+    ]))
+    .pipe(gulp.dest("."));
+})
+
+gulp.task("sprite", function() {
+  return gulp.src("img/icon-*.svg")
+    .pipe(svgstore({
+      inlineSvg: true
+    }))
+    .pipe(rename("sprite.svg"))
+    .pipe(gulp.dest("img"));
+})
+
+gulp.task("images", function() {
+  return gulp.src("img/**/*.{jpg,png,svg}")
+    .pipe(imagemin([
+      imagemin.jpegtran({progressive: true}),
+      imagemin.optipng({optimizationLevel: 3}),
+      imagemin.svgo()
+    ]))
+    .pipe(gulp.dest("img"));
+});
+
+gulp.task("webp", function() {
+  return gulp.src("img/**/*.{jpg,png}")
+    .pipe(imageminWebp({quality: 90}))
+    .pipe(gulp.dest("img"));
+});
 
 gulp.task("style", function() {
   gulp.src("sass/style.scss")
@@ -14,6 +54,9 @@ gulp.task("style", function() {
     .pipe(postcss([
       autoprefixer()
     ]))
+    .pipe(gulp.dest("css"))
+    .pipe(minify())
+    .pipe(rename("style.min.css"))
     .pipe(gulp.dest("css"))
     .pipe(server.stream());
 });
